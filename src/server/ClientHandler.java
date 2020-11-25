a833b39b3d91af43f13edf4d156b1dd8d230a4ca
klotoz <ikskulfg@gmail.com>
2020-11-25 17:36:38 +0300

    start pack

---
+++ src/server/ClientHandler.java
@@ -0,0 +1,86 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+
+    Server server = null;
+    Socket socket = null;
+    DataInputStream in;
+    DataOutputStream out;
+    private String nickname;
+
+
+    public ClientHandler(Server server, Socket socket) {
+        try {
+            this.server = server;
+            this.socket = socket;
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(()-> {
+                    try {
+                        // цикл аутентификации
+                        while (true){
+                            String str = in.readUTF();
+
+                            if (str.startsWith("/auth")){
+                                String[] token = str.split("\\s");
+                                String newNick = server.getAuthService().getNicknameByLoginAndPassword(token[1], token[2]);
+
+                                if (newNick != null){
+                                    nickname = newNick;
+                                    sendMsg("/authok " + nickname);
+                                    server.subscribe(this);
+                                    System.out.println("Клиент " + nickname + " подключился");
+                                    break;
+                                }else {
+                                    sendMsg("Неверный логин / пароль");
+                                }
+                            }
+                        }
+
+                        while (true) {
+                            String str = in.readUTF();
+
+                            if (str.equals("/end")) {
+                                out.writeUTF("/end");
+                                break;
+                            }
+
+                            else {
+                                server.broadCastMsg(this, str);
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }finally {
+                        System.out.println("Клиент отключился");
+                        server.unsubscribe(this);
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+            }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNickname(){
+        return nickname;
+    }
+}
---
+++ src/server/Server.java
@@ -0,0 +1,62 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.Vector;
+
+public class Server {
+
+    private static int PORT = 8189;
+    ServerSocket server = null;
+    Socket socket = null;
+    List<ClientHandler> clients;
+    private AuthService authService;
+
+    public Server() {
+        clients = new Vector<>();
+        authService = new SimpleAuthService();
+
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Сервер запущен");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился");
+                new ClientHandler(this, socket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    void broadCastMsg(ClientHandler sender, String msg) {
+        String message = String.format("%s : %s", sender.getNickname(), msg);
+        for (ClientHandler client : clients) {
+            client.sendMsg(message + "\n");
+        }
+    }
+
+
+
+    public void subscribe(ClientHandler clientHandler){
+        clients.add(clientHandler);
+    }
+
+    public void unsubscribe(ClientHandler clientHandler){
+        clients.remove(clientHandler);
+    }
+
+    public AuthService getAuthService(){
+        return authService;
+    }
+
+}
