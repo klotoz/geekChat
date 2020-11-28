ebf1c755bfa3edb0f2dc7e897714f7cc006d59a2
klotoz <ikskulfg@gmail.com>
2020-11-28 22:53:49 +0300

    add 5, modified 4

--- src/server/AuthService.java
+++ src/server/AuthService.java
@@ -6,4 +6,6 @@ public interface AuthService {
      * @return - nickname если пользоатель есть и null если пользователя нет
      */
     String getNicknameByLoginAndPassword(String login, String password);
+
+    boolean registration(String login, String password, String nickname);
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -3,12 +3,14 @@ package server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Vector;
 
 public class Server {
 
-    private static int PORT = 8189;
+    private static int PORT = 8188;
     ServerSocket server = null;
     Socket socket = null;
     List<ClientHandler> clients;
@@ -39,24 +41,58 @@ public class Server {
     }
 
     void broadCastMsg(ClientHandler sender, String msg) {
-        String message = String.format("%s : %s", sender.getNickname(), msg);
+        SimpleDateFormat formater = new SimpleDateFormat("HH:mm:ss");
+
+        String message = String.format("%s %s : %s", formater.format(new Date()), sender.getNickname(), msg);
         for (ClientHandler client : clients) {
             client.sendMsg(message + "\n");
         }
     }
 
-
-
     public void subscribe(ClientHandler clientHandler){
         clients.add(clientHandler);
+        broadClientList();
     }
 
     public void unsubscribe(ClientHandler clientHandler){
         clients.remove(clientHandler);
+        broadClientList();
     }
 
     public AuthService getAuthService(){
         return authService;
     }
 
+    public void privateCastMsg(ClientHandler sender, String receiver,  String msg) {
+        String message = String.format("[%s] private [%s] : %s", sender.getNickname(), receiver, msg);
+        for (ClientHandler c : clients) {
+            if(c.getNickname().equals(receiver)){
+                c.sendMsg(message + "\n");
+                if(!c.equals(sender)) {
+                    sender.sendMsg(message);
+                }
+                return;
+            }
+        }
+    }
+
+    public boolean isLoginAuthenticated(String login){
+        for (ClientHandler c: clients) {
+            if(c.getLogin().equals(login)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void broadClientList() {
+        StringBuilder sb = new StringBuilder("/clientList ");
+        for(ClientHandler c: clients){
+            sb.append(c.getNickname()).append(" ");
+        }
+        String msg = sb.toString();
+        for (ClientHandler c: clients){
+            c.sendMsg(msg);
+        }
+    }
 }
